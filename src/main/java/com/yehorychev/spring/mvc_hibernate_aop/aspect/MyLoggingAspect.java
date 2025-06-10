44b5fb73d5ddd45b834ac2c14f3c78b5fb799cd6
Yehor Yehorychev <egor57899@gmail.com>
2025-06-10 12:30:59 -0700

    aroundAllRepositoryMethodsAdvice has been created and added

---
+++ src/main/java/com/yehorychev/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,24 @@
+package com.yehorychev.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.yehorychev.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println(methodName + " starts..");
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println(methodName + " ends!");
+
+        return targetMethodResult;
+    }
+}
