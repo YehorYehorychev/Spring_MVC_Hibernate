ad883a553b90f7de721da1996ea94d6b4b49f4be
Yehor Yehorychev <egor57899@gmail.com>
2025-06-12 09:50:21 -0700

    Spring REST | Global Exception Handler has been implemented

--- spring_rest/src/main/java/com/yehorychev/spring/rest/controller/MyRESTController.java
+++ spring_rest/src/main/java/com/yehorychev/spring/rest/controller/MyRESTController.java
@@ -1,9 +1,11 @@
 package com.yehorychev.spring.rest.controller;
 
 import com.yehorychev.spring.rest.entity.Employee;
+import com.yehorychev.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.yehorychev.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id: " +
+                    id + " in Database. Please try to use different id!");
+        }
+
         return employee;
     }
 }
