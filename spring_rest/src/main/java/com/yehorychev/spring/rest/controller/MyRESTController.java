a32602c979e117c2b49c99bb30bfb0e21465bc15
Yehor Yehorychev <egor57899@gmail.com>
2025-06-13 16:24:31 -0700

    Spring REST | POST create new employee api has been created

--- spring_rest/src/main/java/com/yehorychev/spring/rest/controller/MyRESTController.java
+++ spring_rest/src/main/java/com/yehorychev/spring/rest/controller/MyRESTController.java
@@ -4,10 +4,7 @@ import com.yehorychev.spring.rest.entity.Employee;
 import com.yehorychev.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.yehorychev.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,12 @@ public class MyRESTController {
                     id + " in Database. Please try to use different id!");
         }
 
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
         return employee;
     }
 }
